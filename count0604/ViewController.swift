621a27d97f3f7b327ff4148a487675034b21a8cf
福島達也 <t.mugen282@gmail.com>
2016-06-04 15:29:56 +0900

    initial commit

--- count0604/ViewController.swift
+++ count0604/ViewController.swift
@@ -9,6 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var number : Int = 0
+    
+    @IBOutlet var label : UILabel!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +25,22 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func  plus(){
+    number = number + 1
+    label.text = String(number)
+    }
+    @IBAction func  minus(){
+    number = number - 1
+    label.text = String(number)
+    }
+    @IBAction func  waru(){
+    number = number * 2
+    label.text = String(number)
+    }
+    @IBAction func  kakeru(){
+    number = number / 2
+    label.text = String(number)
+    }
 
 }
 
